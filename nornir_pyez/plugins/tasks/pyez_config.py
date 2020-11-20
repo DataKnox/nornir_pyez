1098ae0d7652ad2b860bb9ad12f148861249fe26
Knox Hutchinson <knox@knoxsdata.com>
2020-11-20 08:55:31 +0000

    add support for Jinja2 templates

--- nornir_pyez/plugins/tasks/pyez_config.py
+++ nornir_pyez/plugins/tasks/pyez_config.py
@@ -8,9 +8,11 @@ from nornir_pyez.plugins.connections import CONNECTION_NAME
 
 def pyez_config(
     task: Task,
-    payload: str,
+    payload: str = None,
     update: bool = False,
     data_format: str = 'text',
+    template_path: str = None,
+    template_vars: str = None,
     commit_now: bool = False
 ) -> Result:
 
@@ -18,16 +20,20 @@ def pyez_config(
     device.timeout = 300
     config = Config(device)
     config.lock()
-    if data_format == 'text':
-        if update:
-            config.load(payload, format='text', update=True)
-        else:
-            config.load(payload, format='text', update=False)
+    if template_path:
+        config.load(template_path=template_path,
+                    template_vars=template_vars, format=data_format)
     else:
-        if update:
-            config.load(payload, format=data_format, update=True)
+        if data_format == 'text':
+            if update:
+                config.load(payload, format='text', update=True)
+            else:
+                config.load(payload, format='text', update=False)
         else:
-            config.load(payload, format=data_format, update=False)
+            if update:
+                config.load(payload, format=data_format, update=True)
+            else:
+                config.load(payload, format=data_format, update=False)
     if commit_now:
         if config.commit_check() == True:
             config.commit()
