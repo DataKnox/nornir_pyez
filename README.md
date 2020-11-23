526793e3d5d7fba563d9f9eeb18dcf0ed68ab2d9
KnoxHutchinson <knox@knoxsdata.com>
2020-11-23 15:26:08 +0000

    deploy to pip, cleanup setup and reqs

--- README.md
+++ README.md
@@ -8,7 +8,15 @@ Connections - Pyez
 
 This plugin is used to leverage the power of Juniper's PyEZ with the Nornir framework to offer quicker and more stable delivery of network services while also simplifying and abstracting network inventory
 
+## Installation
+
+```
+pip install nornir-pyez==0.0.4
+```
+
 ## Usages
+
+DOCUMENTATION COMING SOON!
 
 pyez get facts:
 
--- setup.py
+++ setup.py
@@ -1,26 +1,29 @@
-from setuptools import setup
+import setuptools
 
 with open('README.md', 'r') as file:
     long_description = file.read()
 
-setup(name='nornir_pyez',
-      version='0.0.1',
-      description='PyEZs library and plugins for Nornir',
-      url='https://github.com/DataKnox/nornir_pyez',
-      author='Knox Hutchinson',
-      author_email='knox@knoxsdata.com',
-      license='MIT',
-      packages=['nornir_pyez'],
-      keywords=['ping', 'icmp', 'network'],
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: System Administrators',
-          'Natural Language :: English'
-      ],
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      install_requires=['xmltodict', 'nornir', 'nornir_utils', 'junos-eznc'],
-      entry_points={  # Optional
-          'nornir.plugins.connections': "pyez = nornir_pyez.plugins.connections:Pyez"
-      },
-      zip_safe=False)
+with open("requirements.txt", "r") as f:
+    INSTALL_REQUIRES = f.read().splitlines()
+
+setuptools.setup(name='nornir_pyez',
+                 version='0.0.4',
+                 description='PyEZs library and plugins for Nornir',
+                 url='https://github.com/DataKnox/nornir_pyez',
+                 packages=setuptools.find_packages(),
+                 author='Knox Hutchinson',
+                 author_email='knox@knoxsdata.com',
+                 license='MIT',
+                 keywords=['ping', 'icmp', 'network'],
+                 classifiers=[
+                     'Development Status :: 5 - Production/Stable',
+                     'Intended Audience :: System Administrators',
+                     'Natural Language :: English'
+                 ],
+                 long_description=long_description,
+                 long_description_content_type='text/markdown',
+                 install_requires=INSTALL_REQUIRES,
+                 entry_points={
+                     'nornir.plugins.connections': "pyez = nornir_pyez.plugins.connections:Pyez"
+                 },
+                 zip_safe=False)
