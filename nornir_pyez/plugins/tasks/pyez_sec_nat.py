68cdab36930e371f18ce9a539c5720f6658793f1
Calvin Remsburg <packetferret@gmail.com>
2020-12-21 17:59:54 -0600

    add parameter to limit rules returned

--- nornir_pyez/plugins/tasks/pyez_sec_nat.py
+++ nornir_pyez/plugins/tasks/pyez_sec_nat.py
@@ -7,20 +7,38 @@ import xmltodict
 import json
 
 
-def pyez_sec_nat_dest(task: Task) -> Result:
+def pyez_sec_nat_dest(
+    task: Task,
+    rule: str = None
+) -> Result:
 
     device = task.host.get_connection(CONNECTION_NAME, task.nornir.config)
-    data = device.rpc.get_destination_nat_rule_sets_information(all=True)
+
+    # check to see if the user has passed the argument 'rule' in the call; defaults to all.
+    if rule is not None:
+        data = device.rpc.get_destination_nat_rule_sets_information(rule_name=rule)
+    else:
+        data = device.rpc.get_destination_nat_rule_sets_information(all=True)
+
     data = etree.tostring(data, encoding='unicode', pretty_print=True)
     parsed = xmltodict.parse(data)
     clean_parse = json.loads(json.dumps(parsed))
 
     return Result(host=task.host, result=clean_parse)
 
-def pyez_sec_nat_src(task: Task) -> Result:
+def pyez_sec_nat_src(
+    task: Task,
+    rule: str = None
+) -> Result:
 
     device = task.host.get_connection(CONNECTION_NAME, task.nornir.config)
-    data = device.rpc.get_source_nat_rule_sets_information(all=True)
+
+    # check to see if the user has passed the argument 'rule' in the call; defaults to all.
+    if rule is not None:
+        data = device.rpc.get_source_nat_rule_sets_information(rule_name=rule)
+    else:
+        data = device.rpc.get_source_nat_rule_sets_information(all=True)
+
     data = etree.tostring(data, encoding='unicode', pretty_print=True)
     parsed = xmltodict.parse(data)
     clean_parse = json.loads(json.dumps(parsed))
