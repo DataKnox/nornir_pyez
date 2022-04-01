a411aa735dc2eeace36f147926fd3300b707a7ee
Marco Minervino <marco.minervino@proton.ch>
2022-04-01 17:43:36 +0200

    added **scpargs

--- nornir_pyez/plugins/tasks/pyez_scp.py
+++ nornir_pyez/plugins/tasks/pyez_scp.py
@@ -1,13 +1,18 @@
 from nornir.core.task import Result, Task
 
 from nornir_pyez.plugins.connections import CONNECTION_NAME
-
+from typing import Dict
 from jnpr.junos.utils.scp import SCP
 
-def pyez_scp(task: Task, file: str, path: str,
-                ) -> Result:
+
+def pyez_scp(
+    task: Task,
+    file: str,
+    path: str,
+    scpargs: Dict = {"Progress": True},
+) -> Result:
     device = task.host.get_connection(CONNECTION_NAME, task.nornir.config)
     device.timeout = 300
-    with SCP(device, progress=True) as scp:
+    with SCP(device, **scpargs) as scp:
         print(scp.put(file, path))
     return Result(host=task.host, result=f"Successfully copied")
