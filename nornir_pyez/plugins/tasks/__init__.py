e2d0bb4550efb3f663ad943780f5d60a43e33081
geg347 <37312097+geg347@users.noreply.github.com>
2022-04-19 16:09:12 +0200

    Fixing typo in __init__.py in tasks

--- nornir_pyez/plugins/tasks/__init__.py
+++ nornir_pyez/plugins/tasks/__init__.py
@@ -9,7 +9,7 @@ from .pyez_int_terse import pyez_int_terse
 from .pyez_route_info import pyez_route_info
 from .pyez_rollback import pyez_rollback
 from .pyez_rpc import pyez_rpc
-from .pyez_scp import pyez_spc
+from .pyez_scp import pyez_scp
 from .pyez_sec_nat import pyez_sec_nat_dest, pyez_sec_nat_src
 from .pyez_sec_policy import pyez_sec_policy
 from .pyez_sec_vpn import pyez_sec_ike, pyez_sec_ipsec
