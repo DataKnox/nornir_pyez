65d9bc306935d0c36085dccd6fad4c8ebb92fe87
geg347 <37312097+geg347@users.noreply.github.com>
2022-05-24 14:58:07 +0200

    Fixing TypeError on rollback_number variable in pyez_rollback

--- nornir_pyez/plugins/tasks/pyez_rollback.py
+++ nornir_pyez/plugins/tasks/pyez_rollback.py
@@ -6,7 +6,7 @@ from nornir.core.task import Result, Task
 from nornir_pyez.plugins.connections import CONNECTION_NAME
 
 
-def pyez_rollback(task: Task, rollback_number: int = None) -> Result:
+def pyez_rollback(task: Task, rollback_number: int = 0) -> Result:
     device = task.host.get_connection(CONNECTION_NAME, task.nornir.config)
     device.timeout = 300
     config = Config(device)
@@ -15,6 +15,6 @@ def pyez_rollback(task: Task, rollback_number: int = None) -> Result:
     return Result(
         host=task.host,
         result="Successfully emptied config DB"
-        if rollback_number == 0 or rollback_number == None
-        else f"Successfully rollbacked by {rollback_number}",
+        if rollback_number == 0
+        else f"Successfully rollbacked {rollback_number} commit/s",
     )
