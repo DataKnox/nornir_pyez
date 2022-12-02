cf0c0c33d7572b9687a9b3e2805157173b2caac0
penfold1972 <24441474+penfold1972@users.noreply.github.com>
2022-12-02 11:05:02 -0500

    refactor pyez_chassis_inventory.py
    Added QFX command options and refactored file

--- nornir_pyez/plugins/tasks/pyez_chassis_inventory.py
+++ nornir_pyez/plugins/tasks/pyez_chassis_inventory.py
@@ -9,14 +9,25 @@ import json
 
 def pyez_chassis_inventory(
     task: Task,
+    clei-models: str = False,
+    detail: str = False,
+    extensive: str = False,
     models: str = False
 ) -> Result:
 
     device = task.host.get_connection(CONNECTION_NAME, task.nornir.config)
-    if models:
-        data = device.rpc.get_chassis_inventory(models=True)
-    else:
-        data = device.rpc.get_chassis_inventory(models=False)
+
+    cmd_args = {}
+    if clei-models is True:
+        cmd_args["clei-models"]=clei-models
+    if detail is True:
+        cmd_args["detail"]=detail
+    if extensive is True:
+        cmd_args["extensive"]=extensive
+    if models is True:
+        cmd_args["models"]=models
+
+    data = device.rpc.get_chassis_inventory(**cmd_args)
     data = etree.tostring(data, encoding='unicode', pretty_print=True)
     parsed = xmltodict.parse(data)
     clean_parse = json.loads(json.dumps(parsed))
